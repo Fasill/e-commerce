0c5d38fb40f3add40d61ca157b95ddd0875ebafc
fasil <fasilhawultie19@gmail.com>
2023-12-13 13:40:45 +0300

    modification: modification on transition page animation

--- src/pages/transition/transition.js
+++ src/pages/transition/transition.js
@@ -21,8 +21,12 @@ const Shop = () => {
             <h1 
             data-aos="fade-up"
               className='font-bold text-[45px] text-right'>How we're keeping you safe during COVID-19</h1>
-            <p1 className='text-[18px] text-right w-[24rem] justify-self-end' >As an outdoor shop, we’ve taken precautionary measures to ensure the safety of all our customers and team members.</p1>
-            <button className='bg-white rounded text-black font-bold w-[211px] h-[36px] justify-self-end hover:bg-[rgb(235,87,87)] hover:text-white transition-all duration-300'>Read Our Statement</button>
+            <p1 
+             data-aos="fade-up"
+            className='text-[18px] text-right w-[24rem] justify-self-end' >As an outdoor shop, we’ve taken precautionary measures to ensure the safety of all our customers and team members.</p1>
+            <button
+             data-aos="fade-up"
+             className='bg-white rounded text-black font-bold w-[211px] h-[36px] justify-self-end hover:bg-[rgb(235,87,87)] hover:text-white transition-all duration-300'>Read Our Statement</button>
 
         </div>
         </div>
